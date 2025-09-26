f8e1f8cd52ebc5e04a64c3096fb578cb4ddc2139
intel1337@win <taiyang.baluc@laplateforme.io>
2025-09-26 15:07:13 +0200

    Final

--- main.cpp
+++ main.cpp
@@ -4,20 +4,62 @@
 using std::cout;
 using std::cin;
 
-int main(){
+namespace one{
     double a, b, c;
- 
-    cout << "Value A : \n";
-    cin >> a;
+}
+namespace two {
+    double a, b, c;
+}
 
-    cout << "Value B : \n";
-    cin >> b;
-    a = pow(a,2);
-    b = pow(b,2);
-    c = a + b;
-    c = sqrt(c);
-    cout << "Value C : \n";
-    cout << c << '\n';
+int main(){
+    cout << " 1 : Pythagorean\n";
+    cout << " 2 : Pythagorean reciprocal\n";
+    cout << "Choose your method :";
+    
+    std::string input;
+    std::getline(cin >> std::ws, input);
 
+    if(input == "1"){
+        using namespace one;
+        cout << "Value A : \n";
+        cin >> a;
+
+        cout << "Value B : \n";
+        cin >> b;
+
+        a = pow(a,2);
+        b = pow(b,2);
+
+        c = a + b;
+
+        c = sqrt(c);
+
+        cout << "Value C : \n";
+        cout << c << '\n';
+    }
+    else if (input == "2"){
+        using namespace two;
+        cout << "Hypothenus Value : \n";
+        cin >> c;
+        c = pow(c,2);
+
+        cout << "Value A : \n";
+        cin >> a;
+
+        cout << "Value B : \n";
+        cin >> b;
+        double sum = pow(a,2) + pow(b,2);
+        if(sum == c){
+            cout << "It's a right angled Triangle\n";
+        }
+        else{
+            cout << "it is not a right angled triangle\n";
+        }
+    
+    }
+    else{
+        cout << "Wrong input\n";
+        main();
+    }
     return 0;
 }
